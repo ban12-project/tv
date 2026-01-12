81e39a305d245de93553b304b16ca8a42f5f2b6e
Coda <o4oban@gmail.com>
2026-01-12 11:27:36 +0800

    Replace loading placeholder with Skeleton component for improved loading state in color scheme toggle

--- components/color-scheme-toggle-client.tsx
+++ components/color-scheme-toggle-client.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default dynamic(() => import("./color-scheme-toggle"), {
   ssr: false,
-  loading: () => <div className="w-18.5 h-6.5"></div>,
+  loading: () => <Skeleton className="w-18.5 h-6.5 rounded-full"></Skeleton>,
 });
